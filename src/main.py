1e9883d2de76c4b713fb096213ee3d560867b6ee
Yarin <yarinudi@mail.tau.ac.il>
2022-04-29 10:55:07 +0300

    Updates

--- src/main.py
+++ src/main.py
@@ -70,6 +70,7 @@ class ClientServerApp(App):
             f.writelines(f"{self.user_id}\n\n")
             print("ADDED uid!")
 
+        self.build()
         self.init_sensors()
         # self.email()
 
@@ -129,6 +130,7 @@ class ClientServerApp(App):
         """ setup sensors """
         try:
             accelerometer.enable()
+            print('accelerometer enabled')
 
         except:
             print('cant enable accelerometer')
